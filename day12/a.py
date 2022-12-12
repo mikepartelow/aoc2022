d9e7f13fd13c8e8eab9d7b22e0a60309a4345eb5
Mike Partelow <github@mikepartelow.com>
2022-12-12 08:41:13 -0800

    coding is easier

--- day12/a.py
+++ day12/a.py
@@ -26,7 +26,7 @@ def find_start_and_end(hmap):
 def solve(hmap, part2=False):
     """Solve it."""
 
-    # use Djikstra's algo to compute the length of the shortest path
+    # use Dijkstra's algo to compute the length of the shortest path
     # from end to start (not start to end!)
     heights = []
     for row in hmap.splitlines():
